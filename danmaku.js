b676839ce921b8067de2eb881296e4d8cc879b92
wagtail616 <109149349+wagtail616@users.noreply.github.com>
2023-05-22 21:40:08 +0900

    SceneClassの実装

--- danmaku.js
+++ danmaku.js
@@ -30,3 +30,25 @@ class Fighter extends SpriteActor{
         if(input.getKey('ArrowLeft')){this.x-=this.speed}
     }
 }
+class DanmakuStgMainScene extends Scene {
+    constructor(renderingTarget){
+        super('メイン','black',renderingTarget);
+        const fighter = new Fighter(150,300);
+        this.add(fighter);
+    }    
+}
+
+class DanmakuStgTitleScene extends Scene {
+    constructor(renderingTarget){
+        super('タイトル','black',renderingTarget);
+        const title = new Title(100,200);
+        this.add(title);
+    }
+    update(gameInfo,input){
+        super.update(gameInfo,input);
+        if(input.getKeyDown(' ')){
+            const mainScene = new DanmakuStgMainScene(this.renderingTarget);
+            this.changeScene(mainScene);
+        }
+    }
+}
